626abf3a7a9861bb4a027d9d335533a375d0dae8
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-14 10:18:44 +0530

    conditional redering using ternery operator

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
 
 function App() {
-  const name = "Alice";
-  const buttonLable = "Click me"
-  const imgUrl = 'https://placehold.co/50x50';
+  // const name = "Alice";
+  // const buttonLable = "Click me"
+  // const imgUrl = 'https://placehold.co/50x50';
+  const isLogged = false;
+  const element = <h1>{isLogged ? "Welcome back !" : "Please Login to continue"}</h1>
+  const message = [1,"message", "message2"];
   return (
+    // <div>
+    //   <h1>Hello, {name}</h1>
+    //   <button style={{background:"green", color:"white"}} onClick={() => alert(`Button clicked!`)}>{buttonLable}</button>
+    //   <img src={imgUrl}/>
+    // </div>
     <div>
-      <h1>Hello, {name}</h1>
-      <button style={{background:"green", color:"white"}} onClick={() => alert(`Button clicked!`)}>{buttonLable}</button>
-      <img src={imgUrl}/>
+      {element}
+      {message.length > 0 && <h2>You have unread "{message.length}" messages</h2>}
     </div>
+
 
   )
 }
