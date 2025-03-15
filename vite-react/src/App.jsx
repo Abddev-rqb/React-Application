847c980622cb8fcdcaa509880b2923a8019e6494
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-15 17:26:58 +0530

    useRef and useState counters Status:Done

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,23 +1,28 @@
-import About from './components/About';
-import Header from './components/Header';
-import Project from './components/Project';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import { useState, useRef, useEffect} from 'react'
 import './App.css'
 
 function App() {
-  const name = "Abdul Raqeeb";
-  const prof = "Software Architect";
+  const [stateCount, setStateCount] = useState(0);
+  const refCount = useRef(0);
+
+  useEffect(()=>{
+    console.log('component Re-Rendered')
+  });
+  const increStateCount = () =>{
+    setStateCount(stateCount +1);
+  };
+  const increRefCount = ()=>{
+    refCount.current += 1;
+    console.log(`Ref count: ${refCount.current}`);
+  };
 
   return (
-    <div className='App'>
-      <Header />
-      <About />
-      <Project />
-      <Contact />
-      <Footer />
-
-      </div>
+    <div>
+      <p>State count: {stateCount}</p>
+      <button onClick={increStateCount}>Increment State Count</button>
+      <p>State count: {refCount.current}</p>
+      <button onClick={increRefCount}>Increment Ref Count</button>
+    </div>
   )
 }
 
