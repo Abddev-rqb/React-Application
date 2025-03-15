5b74af45bf26abbb9aa545111c0eae1e931eedf2
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-15 12:00:58 +0530

    Form Data with sudo fields project Status:'Done'.

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,67 +1,89 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const name = "Abdul Raqeeb";
-  const prof = "Software Architect";
-  const projects = [
-  {
-    title: "Project One",
-    description: "A project with PHP at the backEnd",
-    link: "#"
-  },
-  {
-    title: "Project Two",
-    description: "A project with JAVA spring boot FullStack",
-    link: "#"
-  },
-  {
-    title: "Project Three",
-    description: "A project with REACT at frontEnd",
-    link: "#"
-  }
-  ];
-  return (
-    <div className='App'>
-      {/* {Header Section} */}
-      <header className='header'>
-        <h1>{name}</h1>
-        <p>{prof}</p>
-        <nav>
-          <a href='#About'>About</a>
-          <a href='#projects'>Projects</a>
-          <a href='#contact'>Contact</a>
-        </nav>
-      </header>
-      {/* {About Section} */}
-      <section id='about' className='about-section'>
-        <h2>About Me</h2>
-        <p>Hello! I am {name}, A passionate {prof} I love building web application that solves real
-          world user problems.
-        </p>
-      </section>
-      {/* {Project Section} */}
-      <section id='project' className='projects-section'>
-        <h2>Projects</h2>
-        <div className='projects-list'>
-          {projects.map((project, index) =>(
-            <div key={index} className='project-items'>
-              <h3>{project.title}</h3>
-              <h3>{project.description}</h3>
-              <a href={project.link} target='_blank' rel="nooponer noreferrer">View Project</a>
-            </div>
-          ))}
-        </div>
-      </section>
-      {/* {Contact Section} */}
-      <section id='contact' className='contact-section'>
-        <h2>Contact Me</h2>
-        <p>If like to get in touch, feel free to email at <a href='#'> abdulsameer.rq61@gmail.com</a></p>
-      </section>
+  const [formData, setFormData] = useState({
+    text:'',
+    checkBox: false,
+    radio: '',
+    select:''
+  });
+  const handleChange = (e) =>{
+    const {name, value, type, checked} = e.target;
+    setFormData({...formData, 
+      [name]:type === 'checkbox'? checked : value
+    });
+  };
 
-      {/* {Footer Section} */}
-      <section className='footer'>
-        <p>2025. All Rights Reserved</p>
-      </section>
+  return (
+    <div className = 'form-container'>
+      <h1>Form Data</h1>
+      <form>
+        {/* Text container */}
+        <div className='form-field'>
+          <label>Text:</label>
+          <input 
+            type = 'text'
+            name = 'text'
+            value = {formData.text}
+            onChange={handleChange}
+          />
+        </div>  
+        <div className='form-field'>
+          <label>
+          <input 
+            type = 'checkBox'
+            name = 'checkBox'
+            checked = {formData.checkBox}
+            onChange={handleChange}
+          />
+          Checkout
+          </label>
+          <div className='form-field'>
+              <label>Radio:</label>
+              <label>
+                <input 
+                  type = 'radio'
+                  name = 'radio'
+                  value= 'option1'
+                  checked = {formData.radio === 'option1'}
+                  onChange={handleChange}
+                />
+                Option 1
+              </label>
+              <label>
+                <input 
+                  type = 'radio'
+                  name = 'radio'
+                  value= 'option2'
+                  checked = {formData.radio === 'option2'}
+                  onChange={handleChange}
+                />
+                Option 2
+              </label>
+            </div>
+            <div className='form-field'>
+              <label>Select:</label>
+              <select 
+                name="select"
+                value = {formData.select}
+                onChange = {handleChange}
+                >
+                  <option value="">--Choose an option--</option>
+                  <option value="option1">Option-1</option>
+                  <option value="option2">Option-2</option>
+              </select>
+            </div>
+            <div className='form-data'>
+              <h3>Form Data</h3>
+              <p><strong>Text:</strong> {formData.text || 'N/A'}</p>
+              <p><strong>CheckOut:</strong> {formData.checkBox ? 'Checked':'Unchecked'}</p>
+              <p><strong>Radio:</strong> {formData.radio || 'N/A'}</p>
+              <p><strong>Select:</strong> {formData.select || 'N/A'}</p>
+            </div>
+        </div>
+      </form>
+
     </div>
   )
 }
