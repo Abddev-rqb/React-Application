41a85833d18e7586e8776fe529b6c9d22816b1d7
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-15 14:48:54 +0530

    Color picker project Status:'Done'

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,23 +1,32 @@
-import About from './components/About';
-import Header from './components/Header';
-import Project from './components/Project';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const name = "Abdul Raqeeb";
-  const prof = "Software Architect";
+  const [backgroundColor, setBackgroundColor] = useState('#ffffff');
+  const colors =['	#b50102', '#8c1d23', '#751433', '#5b1c56', '	#281d46'];
+  const handleColorChange = (color) => {
+    setBackgroundColor(color);
+  }
 
   return (
-    <div className='App'>
-      <Header />
-      <About />
-      <Project />
-      <Contact />
-      <Footer />
-
+    <div style={{backgroundColor}} className='App'>
+      <h1>Color Picker</h1>
+      <div className='color-palatte'>
+        {colors.map((color, index) =>(
+          <div
+            key={index}
+            className='color-box'
+            style={{backgroundColor: color}}
+            onClick = {() => handleColorChange(color)}
+          ></div>
+        ))}
       </div>
+      <div className='custom-color-picker'>
+        <input type = 'color'
+               value = {backgroundColor}
+               onChange = {(e) => handleColorChange(e.target.value)}/>
+      </div>
+    </div>
   )
 }
 
