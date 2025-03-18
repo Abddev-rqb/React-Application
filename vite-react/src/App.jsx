0e48096e09ba4d5de656b50135d3322ae358c765
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-18 19:23:38 +0530

    API Fetch using redux

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,23 +1,11 @@
-import About from './components/About';
-import Header from './components/Header';
-import Project from './components/Project';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 import './App.css'
+import PostList from './PostList'
 
 function App() {
-  const name = "Abdul Raqeeb";
-  const prof = "Software Architect";
-
   return (
-    <div className='App'>
-      <Header />
-      <About />
-      <Project />
-      <Contact />
-      <Footer />
-
-      </div>
+    <>
+      <PostList />
+    </>
   )
 }
 
